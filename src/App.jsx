fd1e2edc412848a2bc7b8300b1fa76b8044aab99
franklin-raph <igboekwulusifranklin@gmail.com>
2024-01-17 18:12:41 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import "./App.css"
-import Navabr from "./components/navbar/Navabr"
+import AuthenticatedNavbar from "./components/navbar/AuthenticatedNavbar"
 import { HashRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import { useEffect, useState } from "react"
+import UnAuthenticatedNavabar from "./components/navbar/UnAuthenticatedNavabar"
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const user = localStorage.getItem("user")
+
+  useEffect(() => {
+    if(user) setLoggedIn(true)
+    else setLoggedIn(false)
+  },[])
+
   return (
     <>
       <HashRouter>
-        <Navabr />
+       {loggedIn &&  <AuthenticatedNavbar />}
+       {/* {!loggedIn && <UnAuthenticatedNavabar /> } */}
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn}/>}/>
         </Routes>
       </HashRouter>
     </>
---
+++ src/components/navbar/UnAuthenticatedNavabar.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import { Link } from "react-router-dom"
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoChevronDown } from "react-icons/io5";
+import { PiWarningCircle } from "react-icons/pi";
+import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
+import SearchComponent from '../search-component/SearchComponent';
+
+const UnAuthenticatedNavabar = () => {
+  return (
+    <div>
+                <navbar className='flex items-start justify-between py-7 px-12 bg-primary-color text-white text-[15px]'>
+            <div className='flex items-start justify-between gap-8'>
+                <Link to="/">Logo</Link>
+                <ul className='flex items-center gap-8'>
+                    <li>
+                        <Link to="/categories">Categories</Link>
+                        <IoChevronDown />
+                    </li>
+                    <li>
+                        <Link to="/new-listing">New Listing</Link>
+                    </li>
+                    <li>
+                        <Link to="/sell-products">Sell Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/more">More</Link>
+                        <IoChevronDown />
+                    </li>
+                </ul>
+            </div>
+            <div className='nav-right'>
+                <li>
+                    <Link to="/login">Login</Link>
+                    <FaRegCircleUser />
+                </li>
+                <li>
+                    <Link to="/help">Help</Link>
+                    <PiWarningCircle />
+                </li>
+                <li className='bg-secondary-color px-3 py-1 rounded-full'>
+                    <HiOutlineDevicePhoneMobile />
+                    <Link to="/get-app">Get App</Link>
+                </li>
+            </div>
+        </navbar>
+        {/* <SearchComponent /> */}
+    </div>
+  )
+}
+
+export default UnAuthenticatedNavabar
