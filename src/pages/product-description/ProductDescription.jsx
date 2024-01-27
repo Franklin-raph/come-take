969c417b028d61835cabd99535ecf69b42684daf
franklin-raph <igboekwulusifranklin@gmail.com>
2024-01-27 09:55:17 +0100

    update

--- src/pages/product-description/ProductDescription.jsx
+++ src/pages/product-description/ProductDescription.jsx
@@ -74,7 +74,7 @@ const ProductDescription = () => {
 
         <div className="lg:px-12 px-3 py-8 mt-10 gap-20">
             <div className='flex items-start gap-[2rem] flex-col-reverse lg:flex-row'>
-                <div className='w-[95%] lg:w-[20%] px-5 py-10 lg:py-0 lg:min-h-[80vh]' style={{boxShadow:"0px 11px 40px -17px rgba(0, 0, 0, 0.14)"}}>
+                <div className='w-[95%] lg:w-[20%] px-5 py-10 lg:py-0 lg:h-[80vh]' style={{boxShadow:"0px 11px 40px -17px rgba(0, 0, 0, 0.14)"}}>
                     <h1 className="font-[600] text-[18px] lg:text-[20px] mb-8 text-primary-color">Delivery & Returns</h1>
                     <div>
                         <div className='mb-7'>
@@ -111,7 +111,7 @@ const ProductDescription = () => {
                     </div>
                 </div>
 
-                <div className='flex flex-col md:flex-row items-start gap-[0rem] px-5 py-10 lg:py-0 lg:min-h-[80vh]' style={{boxShadow:"0px 11px 40px -17px rgba(0, 0, 0, 0.14)"}}>
+                <div className='flex flex-col md:flex-row items-start gap-[0rem] px-5 py-10 lg:py-0 lg:h-[80vh]' style={{boxShadow:"0px 11px 40px -17px rgba(0, 0, 0, 0.14)"}}>
                     <div className='w-[100%] md:w-[40%] mx-auto hidden md:block'>
                         <img src={selectedImage} alt="" className='w-[270px] h-[270px] mb-5 object-contain' />
                         <div className='flex items-center gap-5'>
