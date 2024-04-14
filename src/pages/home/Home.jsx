97a9b68e84239c264d462e570a62ba1f1a2cc7c2
franklin-raph <igboekwulusifranklin@gmail.com>
2024-04-14 11:09:00 +0100

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -472,7 +472,7 @@ async function getAllProducts(){
             </div>
           </div>
 
-          <div className="hidden lg:block">
+          <div className="hidden ">
             <div className="lg:px-12 px-6 py-12">
               <div className="flex items-center justify-between mb-[20px]">
               <h1 className="font-[600] text-[18px] lg:text-[24px] text-primary-color">Newly Listed Products</h1>
@@ -486,7 +486,7 @@ async function getAllProducts(){
           </div>
 
           {/* MOBILE DESIGN FOR NEWLY LISTED*/}
-          <div className="block lg:hidden">
+          <div className="block">
             <div className="lg:px-12 px-6 lg:py-8 py-6">
               <div className="flex items-center justify-between mb-[20px]">
                 <h1 className="font-[600] text-[18px] lg:text-[24px] text-primary-color">Newly Listed Products</h1>
@@ -500,7 +500,7 @@ async function getAllProducts(){
           </div>
           {/* MOBILE DESIGN FOR NEWLY LISTED*/}
 
-          <div className="hidden lg:block">
+          <div className="hidden ">
             <div className="lg:px-12 px-6 py-8">
               <div className="flex items-center justify-between mb-[20px]">
                 <h1 className="font-[600] text-[18px] lg:text-[24px] text-primary-color">Fairly Used Products</h1>
@@ -514,7 +514,7 @@ async function getAllProducts(){
           </div>
 
           {/* MOBILE DESIGN FOR FAIRLY USED*/}
-          <div className="block lg:hidden">
+          <div className="block ">
             <div className="lg:px-12 px-6 lg:py-8 py-6">
               <div className="flex items-center justify-between mb-[20px]">
                 <h1 className="font-[600] text-[18px] lg:text-[24px] text-primary-color">Fairly Used Products</h1>
@@ -528,7 +528,7 @@ async function getAllProducts(){
           </div>
           {/* MOBILE DESIGN FOR FAIRLY USED*/}
 
-          <div className="hidden lg:block">
+          <div className="hidden">
             <div className="lg:px-12 px-6 py-8">
               <div className="flex items-center justify-between mb-[20px]">
                   <h1 className="font-[600] text-[18px] lg:text-[24px] text-primary-color">Brand New Products...</h1>
@@ -542,7 +542,7 @@ async function getAllProducts(){
           </div>
 
           {/* MOBILE DESIGN FOR BRANDED NEW*/}
-          <div className="block lg:hidden">
+          <div className="block ">
             <div className="lg:px-12 px-6 lg:py-8 py-6">
               <div className="flex items-center justify-between mb-[20px]">
                 <h1 className="font-[600] text-[18px] lg:text-[24px] text-primary-color">Brand New Products</h1>
