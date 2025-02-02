5139fdd2e98f1e9e21f05b72f3e5c028c2a35742
franklin-raph <igboekwulusifranklin@gmail.com>
2025-02-02 13:46:51 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <GoogleOAuthProvider clientId="456249570232-g52u7sg2d6brdugelfju6k2jitjmrdbd.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="456249570232-4fgklfg7nf1pdn7ghtqenpnh1dgg7qdi.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>
   // </React.StrictMode>,
